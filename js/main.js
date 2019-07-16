234924b6469fcd6a8d502b3c0ce4bf2717dfa772
nunosantoswebdesigner <nunosantoswebdesigner@gmail.com>
2019-07-16 09:56:20 +0100

    fixed for next promisse or async await

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 var microphone = document.getElementById('microphone');
 
 microphone.onclick=function(){
-    document.getElementById("info").style.display = "none"; 
 
 	microphone.classList.add("record");
 	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 	recognition.lang = 'pt-PT';
 	recognition.start();
-	operations = {"plus":"+",
-				 "minus":"-",
-				 "multiply":"*",
-				 "multiplied":"*",
-				 "divide":"/",
-				 "divided":"/",
-				 "reminder":"%"}
+	// operations = {"plus":"+",
+	// 			 "minus":"-",
+	// 			 "multiply":"*",
+	// 			 "multiplied":"*",
+	// 			 "divide":"/",
+	// 			 "divided":"/",
+	// 			 "reminder":"%"}
 	
 	recognition.onresult = function(event){
 		var input = event.results[0][0].transcript;
-		for(property in operations){
-			input= input.replace(property, operations[property]);
-        }
+		// for(property in operations){
+		// 	input= input.replace(property, operations[property]);
+        // }
         
 		document.getElementById("output-value").innerText = input;
 		setTimeout(function(){
             evaluate(input);
             console.log(input)
-		},2000000);
+		},2000);
 		microphone.classList.remove("record");
 	}
 	
