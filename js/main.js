a19efa9ac2bdd0131beb7f05cb61407f646467d3
nunosantoswebdesigner <nunosantoswebdesigner@gmail.com>
2019-07-18 12:42:07 +0100

    text position fixed on media queries

--- js/main.js
+++ js/main.js
@@ -6,25 +6,25 @@ microphone.onclick=function(){
 	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 	recognition.lang = 'pt-PT';
 	recognition.start();
-	// operations = {"plus":"+",
-	// 			 "minus":"-",
-	// 			 "multiply":"*",
-	// 			 "multiplied":"*",
-	// 			 "divide":"/",
-	// 			 "divided":"/",
-	// 			 "reminder":"%"}
+	operations = {"plus":"+",
+				 "minus":"-",
+				 "multiply":"*",
+				 "multiplied":"*",
+				 "divide":"/",
+				 "divided":"/",
+				 "reminder":"%"}
 	
 	recognition.onresult = function(event){
 		var input = event.results[0][0].transcript;
-		// for(property in operations){
-		// 	input= input.replace(property, operations[property]);
-        // }
+		for(property in operations){
+			input= input.replace(property, operations[property]);
+        }
         
 		document.getElementById("output-value").innerText = input;
 		setTimeout(function(){
             evaluate(input);
             console.log(input)
-		},2000);
+		},5000);
 		microphone.classList.remove("record");
 	}
 	
